a7d04057a12f5690f1193ad8d42f2f5c2affe247
JessesCUkaegbu <ukaegbujesses@gmail.com>
2026-07-07 09:08:57 +0000

    Resolve: DB Migrations issue

--- app/views.py
+++ app/views.py
@@ -4,6 +4,6 @@ from .models import Product
 
 
 def home(request):
-    products = Product.objects.filter(is_active=True)
-    categories = products.order_by("category").values_list("category", flat=True).distinct()
-    return render(request, "app/index.html", {"products": products, "categories": categories})
+    # products = Product.objects.filter(is_active=True)
+    # categories = products.order_by("category").values_list("category", flat=True).distinct()
+    return render(request, "app/index.html")
